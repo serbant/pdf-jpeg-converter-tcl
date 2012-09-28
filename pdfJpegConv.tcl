7cdb14296fbbf3aff4351437a8599d7f9db9c360
serbant <serbant@gmail.com>
2012-09-28 17:36:57 -0400

    cleaned up variable names

--- pdfJpegConv.tcl
+++ pdfJpegConv.tcl
@@ -21,6 +21,7 @@ exec tclsh86 "$0" ${1+"$@"}
 ###############################################################################
 
 if { [ catch { package require fileutil } caughtError ] } {
+
     set missingLib "tcllib"
     puts "ERROR: $caughtError ...\n
 	  possibly missing library $missingLib ..."
@@ -31,10 +32,11 @@ package require struct::queue
 package require log
 
 if { [ catch { package require Thread } caughtError ] } {
+
     set missingLib "tclthread"
     puts "ERROR: $caughtError ...\n
 	  possibly missing library $missingLib ..."
-    showRequiredLibs $missingLib 
+    showRequiredLibs $missingLib
 }
 
 # globals
@@ -62,9 +64,9 @@ tsv::set sharedLogging isErrorReady 0
 #alert     => 6
 #emergency => 7
 set logThread [thread::create -joinable] {
-    
+
     source thrLogger.tcl
-    
+
 }
 
 thread::send $logThread [list set callMainThread $mainThread]
@@ -76,18 +78,18 @@ array set arrCommonUsg [setUsage]
 
 if ([string match $tcl_platform(platform) "windows"]) {
     array set arrPlatformDefs \
-      [setDefaults "[file normalize $env(appdata)]/pdfJpegConv/pdfJpegConv" \
-		    [file normalize $env(home)] "windows"]
+	[setDefaults "[file normalize $env(appdata)]/pdfJpegConv/pdfJpegConv" \
+	[file normalize $env(home)] "windows"]
     array set arrPlatformUsg \
-	 [setUsage "[file normalize $env(appdata)]/pdfJpegConv/pdfJpegConv" \
-		    [file normalize $env(home)] "windows"]
+	[setUsage "[file normalize $env(appdata)]/pdfJpegConv/pdfJpegConv" \
+	[file normalize $env(home)] "windows"]
 } else {
     array set arrPlatformDefs \
-	   [setDefaults "[file normalize $env(home)]/.pdfJpegConv/pdfJpegConv" \
-			 [file normalize $env(home)] "unix"]
+	[setDefaults "[file normalize $env(home)]/.pdfJpegConv/pdfJpegConv" \
+	[file normalize $env(home)] "unix"]
     array set arrPlatformUsg \
-	      [setUsage "[file normalize $env(home)]/.pdfJpegConv/pdfJpegConv" \
-			 [file normalize $env(home)] "unix"]    
+	[setUsage "[file normalize $env(home)]/.pdfJpegConv/pdfJpegConv" \
+	[file normalize $env(home)] "unix"]
 }
 
 set options [list \
@@ -99,9 +101,9 @@ set options [list \
     [list "maxSizeLogs.arg"       ""            $arrCommonUsg(maxSizeLogsUsg)] \
     [list "logLevel.arg"          ""               $arrCommonUsg(logLevelUsg)] \
     [list "excludeConverted.arg"  ""     $arrPlatformUsg(excludeConvertedUsg)] \
-    [list "excludeFucked.arg"     ""        $arrPlatformUsg(excludeFuckedUsg)] \
+    [list "excludeBusted.arg"     ""        $arrPlatformUsg(excludeBustedUsg)] \
     [list "cleanExcludeConverted"        $arrCommonUsg(cleanExcludeConverted)] \
-    [list "cleanExcludeFucked"              $arrCommonUsg(cleanExcludeFucked)] \
+    [list "cleanExcludeBusted"              $arrCommonUsg(cleanExcludeBusted)] \
     [list "chrSearchProgress.arg" ""      $arrCommonUsg(chrSearchProgressUsg)] \
     [list "chrConvProgress.arg"   ""        $arrCommonUsg(chrConvProgressUsg)] \
     [list "searchRootPaths.arg"   ""      $arrPlatformUsg(searchRootPathsUsg)] \
@@ -115,12 +117,12 @@ set usage ": pdfJpegConv.tcl \[options] ...\noptions:"
 array set arrCmdLineParms [::cmdline::getKnownOptions argv $options $usage]
 alogDebug $logThread "see command line option values below"
 # this next command may be iffy, check to see if one can pass the array directly
-# to the log::logarray command or if one needs to first copy the array to the 
+# to the log::logarray command or if one needs to first copy the array to the
 # thread first
 thread::send -async $logThread {log::logarray debug arrCmdLineParms}
 
 # ok, if we populate the defaults directly in the options list, we can't check
-# the priority between cmdline, config file, and defaults. so we wated a lot of 
+# the priority between cmdline, config file, and defaults. so we wated a lot of
 # time with the arrays but we can still  use the defaults array to populate
 # post check
 # see example
@@ -155,16 +157,13 @@ if {[string length $arrCmdLineParms(errorFile)] == 0} {
 }
 alogDebug $logThread "error logging to $errorFile"
 
-
-
-
-# before this next command we should deal with the log and error files, and 
+# before this next command we should deal with the log and error files, and
 # obviously with the log level suppression
 if {[string length $arrCmdLineParms(gsBinary)] == 0} {
     set gsBinary [fileConfigs getProperty "gsBinary"]
     if { [string match $gsBinary "-1"] || \
-	 [string match $gsBinary "0"] || \
-	 [string length $gsBinary] == 0} {
+	[string match $gsBinary "0"] || \
+	[string length $gsBinary] == 0} {
 	set gsBinary $arrPlatformDefs(gsBinaryDef)
     }
 } else {
@@ -173,9 +172,6 @@ if {[string length $arrCmdLineParms(gsBinary)] == 0} {
 alogDebug $logThread "will try to use $gsBinary for conversion"
 # check for gsBinary: put it on a thread and let it spin, wait for that thread
 # after you've finished dealing with the other command line parms
-
-
-
 
 # begin
 
@@ -197,21 +193,21 @@ if { $debug } {
     [ file normalize [ file nativename $fnExcludeConverted ] ] file..."
 }
 
-if { [ file isfile $fnExcludeFucked ] } {      
-    set fExcludeFucked [ open $fnExcludeFucked r ]
-    set content [ read -nonewline $fExcludeFucked ]
-    set listExcludeFucked [ split $content "\n" ]
+if { [ file isfile $fnExcludeBusted ] } {
+    set fExcludeBusted [ open $fnExcludeBusted r ]
+    set content [ read -nonewline $fExcludeBusted ]
+    set listExcludeBusted [ split $content "\n" ]
     puts "\nINFO: Exclude failed conversion files list in \
-    [ file normalize [ file nativename $fnExcludeFucked ] ]:\n\t \
+    [ file normalize [ file nativename $fnExcludeBusted ] ]:\n\t \
     Some of these files can be unfucked the cutePDF writer..."
-    close $fExcludeFucked
+    close $fExcludeBusted
 }
 
 if { $debug } {
     puts "\nDEBUG: all the [ regsub -all {\*|\.} $ext {} ] files \
     that failed conversion during this session will have their names \
     appended to the \
-    [ file normalize [ file nativename $fnExcludeFucked ] ] file. \
+    [ file normalize [ file nativename $fnExcludeBusted ] ] file. \
     Be sure to remove the ones \
     run through cutePDF for another conversion attempt..."
 }
@@ -221,7 +217,7 @@ if { $debug } {
     puts $fDebug "--- Excluded from conversion, already converted ---"
     puts $fDebug [ join $listExcludeConverted \n ]
     puts $fDebug "\n--- Excluded from conversion, fucked-up pdf formatting ---"
-    puts $fDebug [ join $listExcludeFucked \n ]
+    puts $fDebug [ join $listExcludeBusted \n ]
     puts $fDebug "\--- Attempt conversion during this session ---"
     puts $fDebug [ join $listFiles \n ]
     puts $fDebug "\n--- Failures ---"
@@ -241,30 +237,30 @@ if { ![ llength $listFiles ] } {
     [ regsub -all {\*\.} $ext {} ] files..."
     set countSkipped 0
     set countRemaining [ llength $listFiles ]
-}    
+}
 
 set fExcludeConverted [ open $fnExcludeConverted a+ ]
-set fExcludeFucked [ open $fnExcludeFucked a+ ]
+set fExcludeBusted [ open $fnExcludeBusted a+ ]
 
 foreach myFile $listFiles {
-    
+
     if { [ lsearch -exact -sorted -increasing -dictionary $listExcludeConverted \
-	 $myFile ] != -1 } {
+		$myFile ] != -1 } {
 	puts "\nDEBUG: skipping $myFile, already converted..."
 	puts "INFO: [ incr countSkipped ] files..."
 	puts "INFO: [ incr countRemaining -1 ] files to convert..."
 	continue
     }
-    
-    if { [ lsearch -exact -sorted -increasing -dictionary $listExcludeFucked $myFile ] ne -1 } {
+
+    if { [ lsearch -exact -sorted -increasing -dictionary $listExcludeBusted $myFile ] ne -1 } {
 	puts "\nDEBUG: skipping $myFile, conversion failed once already..."
 	puts "INFO: [ incr countSkipped ] files..."
 	puts "INFO: [ incr countRemaining -1 ] files to convert..."
 	continue
     }
-    
+
     puts "\nINFO: [ file nativename $myFile ]: Now converting..."
-    
+
     if { ![createGsOutput $myFile ] } {
 	puts "INFO: [ file nativename $myFile ]: $errorInfo. aborting conversion..."
 	puts "\nINFO: [ incr countErrors ] conversion errors..."
@@ -275,7 +271,7 @@ foreach myFile $listFiles {
 	}
 	continue
     }
-    
+
     if { ![ execGs $myFile ] } {
 	puts "\nINFO: [ file nativename $myFile ]: $errorInfo. aborting conversion..."
 	puts "\nINFO: [ incr countErrors ] conversion errors..."
@@ -286,19 +282,19 @@ foreach myFile $listFiles {
 	}
 	continue
     }
-    
+
     puts "\nINFO: [ file nativename $myFile ]: converted to JPEG images in \
     [ file nativename [ file rootname $myFile ] ]..."
-    
+
     puts $fExcludeConverted $myFile
     flush $fExcludeConverted
-    
+
     puts "\nINFO: [ expr { [ llength $listFiles ] - [ incr countRemaining -1 ] } ] files processed..."
     puts "INFO: [ expr { [ llength $listFiles ] - $countRemaining - $countErrors } ] files converted..."
     puts "INFO: $countErrors files failed conversions so far..."
     puts "INFO: $countSkipped files skipped..."
     puts "INFO: $countRemaining files to convert..."
-    
+
 }
 
 if { $debug } {
@@ -306,16 +302,14 @@ if { $debug } {
 }
 
 close $fExcludeConverted
-close $fExcludeFucked
+close $fExcludeBusted
 
 puts "\nINFO: done. converted [ expr { [ llength $listFiles ] - $countErrors -$countSkipped } ] \
 	[ regsub -all {\*|\.} $ext {} ] files to jpeg images"
 puts "INFO: $countSkipped files skipped..."
 puts "INFO: $countErrors [ regsub -all {\*|\.} $ext {} ] file conversions failed..."
 
-
 # procedures
-
 
 proc showRequiredLibs { missingLib } {
     puts "This application will not run unless the $missingLib library \n
@@ -331,30 +325,30 @@ proc showRequiredLibs { missingLib } {
 }
 
 proc searchFilesRecurse { dir ext { debug 0 } { progress "#" } } {
-    
+
     global listFiles
-    
+
     if { $debug eq 0 } {
-	    puts -nonewline $progress
+	puts -nonewline $progress
     }
-    
-    set listLocalFiles [ lsort -dictionary [ glob -nocomplain -directory $dir $ext ] ] 
+
+    set listLocalFiles [ lsort -dictionary [ glob -nocomplain -directory $dir $ext ] ]
     if { [llength $listLocalFiles ] && $debug} {
 	puts "\nINFO: found [ llength $listLocalFiles ] \
 	      [ regsub -all {\*|\.} $ext {} ] files in directory $dir"
     }
-	    
-	foreach file [ glob -nocomplain $dir/* ] {
-	
+
+    foreach file [ glob -nocomplain $dir/* ] {
+
 	if { [ file isdirectory $file ] } {
 	    if { $debug } {
 		puts "DEBUG: searching [file normalize [file nativename $file]]..."
 	    }
-	    
+
 	    searchFilesRecurse $file $ext
 	    continue
 	}
-	
+
 	if { [ string equal -nocase [ regsub -all {\*} $ext {} ] [ file extension $file ] ] } {
 	    lappend listFiles [ file normalize $file ]
 	    continue
@@ -370,7 +364,7 @@ proc inputChecks { dir } {
     } else {
 	puts "\nINFO: Invalid command line parameter(s) detected.\n\
 	      $dir is not a valid directory or does not exist..."
-	
+
 	return -code ok 0
     }
 }
@@ -391,47 +385,47 @@ proc Usage {} {
 proc createGsOutput { myFile } {
 
     if { [ file isdir [ file normalize [ file rootname [ file normalize \
-	 [ file nativename $myFile ] ] ] ] ] } {
+		[ file nativename $myFile ] ] ] ] ] } {
 	return -code ok 1
     }
-	
+
     if { ![file exists [file normalize [file rootname [file normalize [file nativename $myFile]]]]] || \
-       { [file exists [file normalize [file rootname [file normalize [file nativename $myFile]]]]] && \
-	 [file isfile [file normalize [file rootname [file normalize [file nativename $myFile]]]]] \
-       } } {
-       
+	    { [file exists [file normalize [file rootname [file normalize [file nativename $myFile]]]]] && \
+	    [file isfile [file normalize [file rootname [file normalize [file nativename $myFile]]]]] \
+	    } } {
+
 	if {[catch {file mkdir [file normalize [file rootname [file normalize [file nativename $myFile]]]]} catchMe]} {
 	    return -code error \
-		   -errorInfo "[file nativename $myFile]: can't create output directory, $catchMe" \
-		   0
+		-errorInfo "[file nativename $myFile]: can't create output directory, $catchMe" \
+		0
 	} else {
 	    return -code ok 1
 	}
     }
 }
 
-proc execGs { gsBinary myFile { debug 0 } { fDebug } fExcludeFucked } {
+proc execGs { gsBinary myFile { debug 0 } { fDebug } fExcludeBusted } {
 
     if { [ catch \
-	 { exec -ignorestderr -- $gsBinary \
-		-dBATCH \
-		-dNOPAUSE \
-		-sDEVICE=jpeg \
-		-dJPEGQ=100 \
-		-sOutputFile=[ file normalize [ file rootname [ file normalize \
-			     [ file nativename $myFile ] ] ] ]\\page\-\%03d.jpg \
-		[ file normalize [ file nativename $myFile ] ] \
-		>& "[ file normalize [ file rootname [ file normalize \
+	    { exec -ignorestderr -- $gsBinary \
+		    -dBATCH \
+		    -dNOPAUSE \
+		    -sDEVICE=jpeg \
+		    -dJPEGQ=100 \
+		    -sOutputFile=[ file normalize [ file rootname [ file normalize \
+		    [ file nativename $myFile ] ] ] ]\\page\-\%03d.jpg \
+		    [ file normalize [ file nativename $myFile ] ] \
+		    >& "[ file normalize [ file rootname [ file normalize \
 		    [ file nativename $myFile ] ] ] ]\\conversion.log" \
-	 } catchMe ] \
-	} {
-	    if { $debug } {
-		puts $fDebug "[ file normalize [ file nativename $myFile ] ]: gs error caught $catchMe\?"
-	    }
-	    puts $fExcludeFucked $myFile
-	    flush $fExcludeFucked
-	
-	    return -code ok 0
+		} catchMe ] \
+    } {
+	if { $debug } {
+	    puts $fDebug "[ file normalize [ file nativename $myFile ] ]: gs error caught $catchMe\?"
+	}
+	puts $fExcludeBusted $myFile
+	flush $fExcludeBusted
+
+	return -code ok 0
     } else {
 	return -code ok 1
     }
@@ -470,43 +464,43 @@ proc alogEmergency {idThread message} {
 }
 
 proc setDefaults {{confPath ""} {homePath ""} {platform ""}} {
-    
+
     if {[string length $confPath] > 0} {
 	set configDef           [list "configDef"           "$confPath\.config"]
 	set logFileDef          [list "logFileDef"             "$confPath\.log"]
 	set errorFileDef        [list "errorFileDef"         "$confPath\.error"]
 	set excludeConvertedDef \
-	[list "convertedFileDef" "$confPath\.converted"]
-	set excludeFuckedDef    [list "fuckedFileDef"       "$confPath\.fucked"]
+	    [list "convertedFileDef" "$confPath\.converted"]
+	set excludeBustedDef    [list "fuckedFileDef"       "$confPath\.fucked"]
 	set searchRootPathsDef  [list "searchRootPathsDef"            $homePath]
 	if {[string match $platform "windows"]} {
 	    set gsBinaryDef \
-	    [list "gsBinaryDef" "C:/Program Files/gs/gs8.70/bin/gswin32c.exe"]
+		[list "gsBinaryDef" "C:/Program Files/gs/gs8.70/bin/gswin32c.exe"]
 	} else {
 	    set gsBinaryDef [list "gsBinaryDef"                   "/usr/bin/gs"]
 	}
 	return -code ok [concat $configDef $logFileDef $errorFileDef \
-	 $excludeConvertedDef $excludeFuckedDef \
-	 $searchRootPathsDef $gsBinaryDef]
+	    $excludeConvertedDef $excludeBustedDef \
+	    $searchRootPathsDef $gsBinaryDef]
     }
-    
+
     set maxSizeLogsDef       [list "maxSizeLogsDef"                     1048576]
     set logLevelDef          [list "logLevelDef"                              0]
     set chrSearchProgressDef \
-    [list "chrSearchProgressDef" "Looking for pdf files..."] 
-    set chrConvProgressDef   [list "chrConvProgressDef"         "Converting..."] 
-    set searchExtDef         [list "searchExtDef"                     "\*\.pdf"] 
+	[list "chrSearchProgressDef" "Looking for pdf files..."]
+    set chrConvProgressDef   [list "chrConvProgressDef"         "Converting..."]
+    set searchExtDef         [list "searchExtDef"                     "\*\.pdf"]
     set minSearchThreadsDef  [list "minSearchThreadsDef"                     10]
     set maxSearchThreadsDef  [list "maxSearchThreadsDef"                     40]
     set minConvThreadsDef    [list "minConvThreadsDef"                        1]
     set maxConvThreadsDef    [list "maxConvThreadsDef"                        8]
     return -code ok [concat $maxSizeLogsDef $logLevelDef $chrSearchProgressDef \
-     $chrConvProgressDef $searchExtDef $minSearchThreadsDef \
-     $maxSearchThreadsDef $minConvThreadsDef $maxConvThreadsDef]
+	$chrConvProgressDef $searchExtDef $minSearchThreadsDef \
+	$maxSearchThreadsDef $minConvThreadsDef $maxConvThreadsDef]
 }
 
 proc setUsage {{confPath ""} {homePath ""} {platform ""}} {
-    
+
     if {[string length $confPath] > 0} {
 	set configUsg [list "configUsg" "get options from this configuration \
 	 file.\ncommand line options take precedence over config \
@@ -520,7 +514,7 @@ proc setUsage {{confPath ""} {homePath ""} {platform ""}} {
 	 list of files to exclude because they have \
 	 already been converted.\n \
 	 default value is $confPath\.converted."]
-	set excludeFuckedUsg [list "excludeFuckedUsg" "file containing a list \
+	set excludeBustedUsg [list "excludeBustedUsg" "file containing a list \
 	 of files to exclude because they have failed \
 	 conversion at least once.\nupdated while running. \
 			     \ndefault value is $confPath\.fucked."]
@@ -537,15 +531,15 @@ proc setUsage {{confPath ""} {homePath ""} {platform ""}} {
 	     GhostScript binary.\ndefault value is /usr/bin/gs."]
 	}
 	return -code ok [concat $configUsg $logFileUsg $errorFileUsg \
-	 $excludeConvertedUsg $excludeFuckedUsg \
-	 $searchRootPathsUsg $gsBinaryUsg]
+	    $excludeConvertedUsg $excludeBustedUsg \
+	    $searchRootPathsUsg $gsBinaryUsg]
     }
-    
+
     set forgetConfigUsg [list "forgetConfigUsg" "do not save the current \
      configuration to the default configuration file."]
     set cleanExcludeConvertedUsg [list "cleanExcludeConvertedUsg" "remove \
      information about files already converted."]
-    set cleanExcludeFuckedUsg [list "cleanExcludeFuckedUsg" "remove \
+    set cleanExcludeBustedUsg [list "cleanExcludeBustedUsg" "remove \
      information about files that have failed \
      conversion."]
     set maxSizeLogsUsg [list "maxSizeLogsUsg" "maximum size of any active log \
@@ -575,44 +569,44 @@ proc setUsage {{confPath ""} {homePath ""} {platform ""}} {
      to use when converting.\ndefault value is 1."]
     set maxConvThreadsUsg [list "maxConvThreadsUsg" "maximum number of threads \
      to use when converting.\ndefault is 8."]
-    
+
     return -code ok [concat $forgetConfigUsg $cleanExcludeConvertedUsg \
-     $cleanExcludeFuckedUsg $maxSizeLogsUsg $logLevelUsg \
-     $chrSearchProgressUsg $chrConvProgressUsg $searchExtUsg \
-     $minSearchThreadsUsg $maxSearchThreadsUsg \
-     $minConvThreadsUsg $maxConvThreadsUsg]
-    
+	$cleanExcludeBustedUsg $maxSizeLogsUsg $logLevelUsg \
+	$chrSearchProgressUsg $chrConvProgressUsg $searchExtUsg \
+	$minSearchThreadsUsg $maxSearchThreadsUsg \
+	$minConvThreadsUsg $maxConvThreadsUsg]
+
 }
 
- proc multiBytes2bytes {bytesString} {
-     # TODO need to check for maxSize 999Mbytes in all cases and only return
-     # the value if lte than that, otherwise just return 1Mb
-     # DONE
-     
-     if {[string is integer -strict -failindex idx $bytesString]} {
-	 set ret $bytesString
-     } else {
-	 switch -nocase [string index $toBytes $idx] {
-	     "b" {
-		 set ret [string range $toBytes 0 [expr {$idx -1 }]]
-	     }
-	     "k" {
-		 set ret [expr {$toByes * 1024}]
-	     }
-	     "m" {
-		 set ret -code ok [expr {$toBytes * 1024 * 1024}]
-	     }
-	     default {
-		 set ret 1048576
-	     }
-	 }
-     }
-     if {$ret >= 1047527424} {
-	 set ret 1048576
-     }
-     
-     return -code ok $ret
- }
+proc multiBytes2bytes {bytesString} {
+    # TODO need to check for maxSize 999Mbytes in all cases and only return
+    # the value if lte than that, otherwise just return 1Mb
+    # DONE
+
+    if {[string is integer -strict -failindex idx $bytesString]} {
+	set ret $bytesString
+    } else {
+	switch -nocase [string index $toBytes $idx] {
+	    "b" {
+		set ret [string range $toBytes 0 [expr {$idx -1 }]]
+	    }
+	    "k" {
+		set ret [expr {$toByes * 1024}]
+	    }
+	    "m" {
+		set ret -code ok [expr {$toBytes * 1024 * 1024}]
+	    }
+	    default {
+		set ret 1048576
+	    }
+	}
+    }
+    if {$ret >= 1047527424} {
+	set ret 1048576
+    }
+
+    return -code ok $ret
+}
 
 exit 0
 
